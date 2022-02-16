39c2ac444e38f7c1d35e2e004008360416396250
Gehu Pierre <info@pierrestudios.com>
2022-02-16 13:43:54 -0500

    Retrieve api var from Rails config

--- lib/sfm_mail.rb
+++ lib/sfm_mail.rb
@@ -1,20 +1,31 @@
+# frozen_string_literal: true
+
 require 'httparty'
 
+# Service to send transactional email via HTTP to SF Marketingcloud Mail
 class SfmMail
   def self.send(mail)
-    @from_name = mail[:from].display_names[0] || ''
-    @from = mail.from[0].gsub(@from_name, '').gsub('\u003c', '').gsub('\u003e', '')
-
+    set_required_props(mail)
     contact_recipient = mail[:to].addresses
     is_multiple_recipient = contact_recipient.length > 1
-    api_path = "messaging/v1/messageDefinitionSends/key:#{ENV['SFM_SEND_KEY']}/#{is_multiple_recipient ? 'sendBatch' : 'send'}"
-    data = SfmMail.get_recipients_data(contact_recipient, {
-                                                   SubscriberAttributes: {
-                                                     BodyContentText: mail.body.raw_source,
-                                                     Subject: mail.subject
-                                                   }
-                                                 })
-    SfmMail.api_request(api_path, is_multiple_recipient ? data : data[0])
+    api_path = "messaging/v1/messageDefinitionSends/key:#{@api_params[:send_key]}/#{is_multiple_recipient ? 'sendBatch' : 'send'}"
+    data = get_recipients_data(contact_recipient, {
+                                 SubscriberAttributes: {
+                                   BodyContentText: mail.body.raw_source,
+                                   Subject: mail.subject
+                                 }
+                               })
+    api_request(api_path, is_multiple_recipient ? data : data[0])
+  end
+
+  def self.set_required_props(mail)
+    unless Rails.application.config.respond_to?(:sfm_mail_options) && Rails.application.config.sfm_mail_options.present?
+      raise 'SfmMail Configuration is missing'
+    end
+
+    @api_params = Rails.application.config.sfm_mail_options
+    @from_name = mail[:from].display_names[0] || ''
+    @from = mail.from[0].gsub(@from_name, '').gsub('\u003c', '').gsub('\u003e', '')
   end
 
   def self.get_recipients_data(recipients, contact_attrs)
@@ -49,21 +60,21 @@ class SfmMail
     }
 
     if data.present?
-      HTTParty.post("#{ENV['SFM_REST_URL']}/#{path}",
+      HTTParty.post("#{@api_params[:rest_url]}/#{path}",
                     body: data.to_json,
                     headers: headers)
     else
-      HTTParty.get("#{ENV['SFM_REST_URL']}/#{path}",
+      HTTParty.get("#{@api_params[:rest_url]}/#{path}",
                    headers: headers)
     end
   end
 
   def self.api_get_token
-    auth = HTTParty.post(ENV['SFM_AUTH_URL'],
+    auth = HTTParty.post(@api_params[:auth_url],
                          body: {
                            'grant_type' => 'client_credentials',
-                           'client_id' => ENV['SFM_CLIENT_ID'],
-                           'client_secret' => ENV['SFM_CLIENT_SECRET'],
+                           'client_id' => @api_params[:client_id],
+                           'client_secret' => @api_params[:client_sec],
                            'scope' => 'email_read email_write email_send'
                          }.to_json,
                          headers: { 'Content-Type' => 'application/json' })
--- sfm-mail.gemspec
+++ sfm-mail.gemspec
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 Gem::Specification.new do |s|
   s.name        = 'sfm_mail'
   s.version     = '1.0.0'
-  s.summary     = "SFM Mail"
-  s.description = "A simple gem to integrate SF Marketingcloud Mail"
-  s.authors     = ["Barefoot"]
+  s.summary     = 'SFM Mail'
+  s.description = 'A simple gem to integrate SF Marketingcloud Mail'
+  s.authors     = ['Barefoot']
   s.email       = 'gehu.pierre@barefootproximity.com'
-  s.files       = ["lib/sfm_mail.rb"]
+  s.files       = ['lib/sfm_mail.rb']
   s.homepage    =
-    'https://rubygems.org/gems/sfm-mail'
-  s.license       = 'MIT'
+    'https://github.com/BarefootProximity/sfm-mail-gem'
+  s.license = 'MIT'
 end
